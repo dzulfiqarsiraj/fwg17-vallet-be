c3639a314bab0260b7f157b19caff85e63bfb9d7
putragabrielll <putragmahk@gmail.com>
2024-01-14 15:10:39 +0700

    update

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
 console.log('Helllo world')
 console.log('gabriel')
-console.log('Haidar was here')
-console.log('Haidar was here')
-console.log('Haidar was here')
-console.log('Haidar was here')
-console.log('Haidar was here')
-console.log('Haidar was here')
