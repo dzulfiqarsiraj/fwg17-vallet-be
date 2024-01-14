2ad79fd955f66305865480676bddd73a5c487291
putragabrielll <putragmahk@gmail.com>
2024-01-14 15:38:04 +0700

    index awal

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-console.log('Helllo world')
-console.log('gabriel')
-console.log('Haidar was here')
-console.log('Haidar was here')
-console.log('Haidar was here')
-console.log('Haidar was here')
-console.log('Haidar was here')
-console.log('Haidar was here')
+require('dotenv').config({
+    patch: './.env'
+})
+
+const express = require('express')
+
+const app = express()
+
+// Respons bila link yg input tidak ada
+app.use('/', (reg, res)=>{
+    res.status(404)
+    res.send('<h1>404</h1>')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`)
+})
