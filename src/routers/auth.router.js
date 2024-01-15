24c2c314c9dc81ad269b95c175255c0ab6507ee8
Dzulfiqar <103058254+DzulfiqarSiraj@users.noreply.github.com>
2024-01-15 12:50:30 +0700

    feat: add auth/register endpoint

--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -3,5 +3,6 @@ const authController = require('../controllers/auth.controller')
 
 
 authRouter.use('/login', authController.login)
+authRouter.use('/register', authController.register)
 
 module.exports = authRouter
