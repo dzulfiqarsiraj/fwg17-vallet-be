39fa8ec2bd658d2257bfffaccc08b32d3dca10e1
Dzulfiqar <103058254+DzulfiqarSiraj@users.noreply.github.com>
2024-01-15 21:14:12 +0700

    feat: add crud admin/wallet

--- src/routers/admin/index.js
+++ src/routers/admin/index.js
@@ -2,5 +2,6 @@ const adminRouter = require('express').Router()
 
 adminRouter.use('/roles', require('./roles.router'))
 adminRouter.use('/users', require('./users.router'))
+adminRouter.use('/wallet', require('./wallet.router'))
 
 module.exports = adminRouter
