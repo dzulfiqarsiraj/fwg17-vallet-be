fbf3ee017f8af64de4759e47d3733899ea50394c
xel26 <zhxo111nvshq@gmail.com>
2024-01-18 01:55:54 +0700

    feature: top-up

--- src/routers/customers/deposit.router.js
+++ src/routers/customers/deposit.router.js
@@ -1,6 +1,7 @@
 const depositRouter = require('express').Router()
 const depositController = require('../../controllers/customers/deposit.controller')
 
+depositRouter.get('/', depositController.getAll)
 depositRouter.post('/', depositController.deposit)
 
 module.exports = depositRouter
