260852a4e49467bb94a60ef34bd42cbf6142310f
putragabrielll <putragmahk@gmail.com>
2024-01-21 13:28:00 +0700

    fiix: search and limit history transactions

--- src/controllers/customers/historyTransactions.controller.js
+++ src/controllers/customers/historyTransactions.controller.js
@@ -8,14 +8,32 @@ const handleErr = require("../../helpers/utils")
 exports.getAllData = async(req, res) => {
     try{
         const {id} = req.user
-        const getContactList = await historyTransactionsModel.allHistoryTransactions(id)
+        const { filter = '', page = 1, limits = 9 } = req.query
+        const countData = await historyTransactionsModel.countData(id, filter)
+        if(countData == 0){
+            throw ({code: "THROW", message: "No Data!"})
+        }
+
+        const getContactList = await historyTransactionsModel.allHistoryTransactions(id, filter, page, limits)
+        
         if(getContactList.length < 1){
             throw ({code: "THROW", message: "No Data!"})
         }
 
+        const totalPage = Math.ceil(countData / limits)
+        const nextPage = Number(page) + 1
+        const prevPage = Number(page) - 1
+
         return res.json({
             success: true,
             message: 'List all History Transactions!',
+            pageInfo: {
+                currentPage: Number(page),
+                totalPage,
+                nextPage: nextPage <= totalPage ? nextPage : null,
+                prevPage: prevPage >= 1 ? prevPage : null,
+                totalData: Number(countData)
+            },
             results: getContactList
         })
 
