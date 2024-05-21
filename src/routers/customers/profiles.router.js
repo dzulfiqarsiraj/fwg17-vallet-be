bac928682efd3a88ff7506b43ea11708c0ade150
Dzulfiqar <103058254+DzulfiqarSiraj@users.noreply.github.com>
2024-05-21 22:26:04 +0700

    feat: add upload middleware

--- src/routers/customers/profiles.router.js
+++ src/routers/customers/profiles.router.js
@@ -1,7 +1,14 @@
 const profileRouter = require("express").Router()
 const profileController = require('../../controllers/customers/profiles.controller')
 
+const uploadMiddleware = require('../../middleware/upload.middleware')
+const multerErrorHandler = require('../../middleware/multerErrorHandler.middleware')
+
 profileRouter.get('/', profileController.getProfile)
-profileRouter.patch('/', profileController.updateProfile)
+profileRouter.patch('/',
+    uploadMiddleware("users").single("picture"),
+    multerErrorHandler,
+    profileController.updateProfile
+)
 
 module.exports = profileRouter
