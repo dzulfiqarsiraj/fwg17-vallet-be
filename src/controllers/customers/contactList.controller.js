368d4b29d84fa91a9932195472843b46f198502d
Dzulfiqar <103058254+DzulfiqarSiraj@users.noreply.github.com>
2024-05-21 22:34:37 +0700

    feat: controller getOneData and pageInfo at getAllData

--- src/controllers/customers/contactList.controller.js
+++ src/controllers/customers/contactList.controller.js
@@ -1,5 +1,4 @@
 const contactListModel = require('../../models/contactList.model')
-const userModel = require('../../models/users.model')
 const errorHandler = require('../../helpers/utils')
 
 // hendle semua error yg terjadi di catch
@@ -9,21 +8,35 @@ const handleErr = require("../../helpers/utils")
 // SELECT *
 exports.getAllData = async (req, res) => {
     try {
+        const {limit="5", phoneNumber, page="1"} = req.query
+        const offset = (page - 1) * limit
         let getContactList
-        if (req.query.phoneNumber) {
+        let count
 
-            const phoneNumber = req.query.phoneNumber
-            getContactList = await userModel.findOneByPhoneNumber(phoneNumber)
+        if (phoneNumber) {
+            count = await contactListModel.countAllByPhoneNumber(phoneNumber)
+            getContactList = await contactListModel.findByPhoneNumber(phoneNumber, limit, offset, page)
         } else {
-
             const { id } = req.user
             const { search = '' } = req.query
+            count = await contactListModel.countAll()
             getContactList = await contactListModel.allContactListforCustomer(id, search)
         }
+
         if (getContactList.length < 1) {
             throw ({ code: "THROW", message: "No Data!" })
         }
-        console.log(req.query.phoneNumber)
+
+        const totalPage = Math.ceil(count / limit)
+
+        const pageInfo = {
+            totalPage: totalPage,
+            currentPage: Number(page),
+            nextPage: page <= totalPage ? Number(page) + 1 : 0,
+            prevPage: page >= 1 ? Number(page) - 1 : 0,
+            totalData: Number(count),
+            limit: Number(limit)
+        }
 
         return res.json({
             success: true,
@@ -33,6 +46,22 @@ exports.getAllData = async (req, res) => {
 
     } catch (err) {
         handleErr.outError(err, res)
+    }
+}
+
+exports.getOneData = async(req, res) => {
+    const {id} = req.params
+
+    try {
+        const user = await contactListModel.findOne(id)
+          return res.json({
+            success: true,
+            message: `Detail user`,
+            results: user
+          })
+
+    } catch (error) {
+        handleErr.outError(error, res)
     }
 }
 
