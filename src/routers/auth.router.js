b30de9dc887a231f369507800d1ccf3dbebb7565
Dzulfiqar <103058254+DzulfiqarSiraj@users.noreply.github.com>
2024-05-21 22:28:03 +0700

    feat: add verify pin, find user by email, and find user by phone router

--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -6,7 +6,10 @@ authRouter.use('/login', authController.login)
 authRouter.use('/register', authController.register)
 authRouter.use('/transfer/:id', authController.verifyPin)
 authRouter.use('/verify-password/:id', authController.verifyPassword)
+authRouter.use('/verify-pin/:id', authController.verifyPin)
 authRouter.post('/forgot-password', authController.forgotPassword)
 authRouter.post('/forgot-pin', authController.forgotPin)
+authRouter.get('/find-user-by-email', authController.findUserByEmail)
+authRouter.get('/find-user-by-phone', authController.findUserByPhone)
 
 module.exports = authRouter
